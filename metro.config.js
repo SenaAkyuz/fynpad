b0366052f3b56396f11c2973d99610e75a988e1e
senaakyuz <senayfl.1209@gmail.com>
2026-05-25 19:25:19 +0300

    Add metro config to fix Supabase+Hermes OTEL dynamic import

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+﻿const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Supabase + Hermes: OTEL dynamic import sorununu önler
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
